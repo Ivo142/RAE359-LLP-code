27cc8eed6daec843b761529a731a040a3c188a23
Ivo142 <42965152+Ivo142@users.noreply.github.com>
2020-07-09 00:11:45 +0300

    Update LLP Example.py

--- LLP Example.py
+++ LLP Example.py
@@ -3,11 +3,14 @@ from multiprocessing import Process, Pipe
 from os import getpid
 from datetime import datetime
 # Local timestamp
+# Ar šīs koda daļas palīdzību galarezultātam tiks pievienota gan lamporta pulksteņa pozīcija, gan procesa pulksteņa vērtība.
 def local_time(counter):
     return '(Lamport_time={}, Local_time={})'. format(counter, datetime.now())
 # New timestamp when a message is recieved
+# Šajā koda daļā tiek ietverta informācija par to, ka izmainīsies laika vērtības, notiekot kādam notikumam.
 def calc_recv_timestamp(recv_time_stamp, counter):
     return max(recv_time_stamp, counter) + 1
+# Nākamajās trīs koda daļās tiek definēti notikumi kādi var notikt starp procesiem, tiek definēts notikums kā tāds, ziņojuma nosūtīšanas notikums un ziņojuma saņemšanas notikums.
 # Funckija visiem iespējamajiem notikumiem
 def event(pid, counter):
     counter += 1
@@ -29,6 +32,9 @@ def recv_message(pipe, pid, counter):
     print('Message recieved at ' + str(pid) + local_time(counter))
     return counter
 # Procesu definēšana
+# Šeit notiek procesu definēšana starp kuriem notiks notikumi. Vispirms kodā tiek ietverts tas ka katram procesam būs savs ID kods.
+# Šeit jāņem vērā arī tas, ka katru reizi kad kods tiek palaists no jauna, katram procesam būs cits ID kods.
+# Tad tiek ierakstīta informācija par katru iespējamo notikumu.
 def process_one(pipe12):
     pid = getpid()
     counter = 0
@@ -50,9 +56,10 @@ def process_three(pipe32):
     pid = getpid()
     counter = 0
     counter = recv_message(pipe32, pid, counter)
-    counter = send_message(pipe32, pid, counte)
+    counter = send_message(pipe32, pid, *counte*)
 
 # Procesu izveide
+# Šajā koda daļā tiek izveidoti paši procesi starp kuriem notiks notikumi.
 if __name__ == '__main__':
 
     oneandtwo, twoandone = Pipe()
@@ -64,7 +71,8 @@ if __name__ == '__main__':
                        args=(twoandone, twoandthree))
     process3 = Process(target=process_three,
                        args=(threeandtwo,))
-
+# Šī koda daļa ir svarīga, jo bez šīs koda daļas kods nestrādās pareizi. Šī koda daļa nodrošina to, lai notiktu procesi kā arī lai tiktu iziets no koda rezultātu loga pēc koda pilnīgas izpildes.
+# Lai būtu iespējams aplūkot rezultātu ir iespējams Piespiest kodam pārtraukt izpildi un uzrādīt kļūdu(Tas ir redzams koda 59 rindiņā)
     process1.start()
     process2.start()
     process3.start()
